028dd4695b11164c4edf7931987f41c36d60d9e7
JazminLuque <luque6170@gmail.com>
2023-09-08 16:54:52 -0300

    Clase alumno
    Agregue atributos, getter and setter y constructor. El ID esta en -1 porque se genera solo con la plataforma

--- src/Entidades/Alumno.java
+++ src/Entidades/Alumno.java
@@ -5,10 +5,82 @@
  */
 package Entidades;
 
-/**
- *
- * @author Kanji
- */
+import java.util.Date;
+
 public class Alumno {
-    private int id;
+    private int idAlumno=-1;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private Date fechaNac;
+    private boolean estado;
+
+    public Alumno(int dni, String apellido, String nombre, Date fechaNac, boolean estado) {
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.estado = estado;
+    }
+
+    public Alumno(int idAlumno, int dni, String apellido, String nombre, Date fechaNac, boolean estado) {
+        this.idAlumno = -1;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.estado = estado;
+    }
+
+    public Alumno() {
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
 }
