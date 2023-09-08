fe0b572df43190c00974acd11e53e48a8e642a98
JazminLuque <luque6170@gmail.com>
2023-09-08 16:57:26 -0300

    clase Inscripcion
    Agregue atributos, constructor y getter and setter.

--- src/Entidades/Inscripcion.java
+++ src/Entidades/Inscripcion.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entidades;
 
-/**
- *
- * @author Kanji
- */
 public class Inscripcion {
-    
+  private int idInscripto=-1;
+private int nota; 
+private int idAlumno;
+private int idMateria;
+
+    public Inscripcion(int idInscripto, int nota, int idAlumno, int idMateria) {
+        this.idInscripto = -1;
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+    }
+
+    public Inscripcion(int nota, int idAlumno, int idMateria) {
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+    }
+
+    public Inscripcion() {
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
 }
