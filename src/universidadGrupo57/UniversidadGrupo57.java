956b3157976f0395a22492bfcbce5ab026b3fc65
JohnDeSouza49 <141689720+JohnDeSouza49@users.noreply.github.com>
2023-09-11 20:20:56 -0300

    gggg

--- src/universidadGrupo57/UniversidadGrupo57.java
+++ src/universidadGrupo57/UniversidadGrupo57.java
@@ -15,7 +15,7 @@ public class UniversidadGrupo57 {
 
     public static void main(String[] args) {
      //Connection con = Conexion.getConexion();  // TODO code application logic here
-    Alumno juan=new Alumno (12312312,"luna","pedro",LocalDate.of(1980,4,25),true);
+    Alumno juan=new Alumno (12312366,"luna","pedro",LocalDate.of(1980,4,25),true);
     AlumnoData alu=new AlumnoData();
     alu.guardarAlumno(juan);
     }
