963bc92c8b14ad9ac2ba8e2d080b9cabc8b329f9
JazminLuque <luque6170@gmail.com>
2023-09-11 19:03:15 -0300

    guardarMateria

--- src/AccesoADatos/MateriaData.java
+++ src/AccesoADatos/MateriaData.java
@@ -3,6 +3,13 @@ package AccesoADatos;
 
 import Entidades.Materia;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class MateriaData {
 private Connection conex= null;
@@ -13,7 +20,25 @@ public MateriaData(){
 
 public void guardarMateria(Materia materia){
     String sql="INSERT INTO materia (nombre,año, estado) VALUES (?, ?, ?)";
+    try {
+        PreparedStatement ps= conex.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, materia.getNombre());
+        ps.setInt(2, materia.getAnio());
+        ps.setBoolean(3, materia.isEstado());
+        ps.executeUpdate();
+        ResultSet id= ps.getGeneratedKeys();
+        if(id.next()){
+         materia.setIdMateria(id.getInt("idMateria"));
+            System.out.println("Materia agregada con exito");
+        }
+        ps.close();
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al agregar Materia");
+    }
+    
+        
+    }
 }
 
 
-}
+
