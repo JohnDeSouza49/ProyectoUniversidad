a9a34a9169a8e9c4d5e42a3168953756c5d27e21
co-der-tand <lucasmartin1@hotmail.com.ar>
2023-09-11 20:45:13 -0300

    Agregamos Metodos
    Agregamos los metodos  vacios

--- src/AccesoADatos/InscripcionData.java
+++ src/AccesoADatos/InscripcionData.java
@@ -1,7 +1,67 @@
-
 package AccesoADatos;
 
+import Entidades.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InscripcionData {
+
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+
+    public InscripcionData() {
+
+        con = Conexion.getConexion();
+    }
+
+    public void guardarInscripcion(Inscripcion insc) {
+
+    }
+
+    public List<Inscripcion> obtenerInscripciones() {
+
+        List<Inscripcion> inscripciones = new ArrayList<>();
+        
+        return inscripciones;
+    }
     
+    public List<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
+
+        List<Inscripcion> inscPorId = new ArrayList<>();
+        
+        return inscPorId;
+    }
+    
+    public List<Materia> obtenerMateriasCursadas(int id){
+        
+        List <Materia> materiasCursadas = new ArrayList<>();
+        
+        return materiasCursadas;
+    }
+    
+    public List<Materia> obtenerMateriasNoCursadas(int id){
+        
+        List <Materia> materiasNoCursadas = new ArrayList<>();
+        
+        return materiasNoCursadas;
+    } 
+    
+     public void borrarInscripcionMaterialAlumno(int idAlumno, int idMateria){
+            
+      
+    }
+     
+     public void actualizarNota(int idAlumno, int idMateria, double nota){
+         
+     }
+     
+      public List<Alumno> obtenerAlumnosXMateria(int idMateria){
+        
+        List <Alumno> materiasXAlumno = new ArrayList<>();
+        
+        return materiasXAlumno;
+    }
+
 }
