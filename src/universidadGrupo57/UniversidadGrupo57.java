d796edb195fa72dde3c1862a0c18ac39caa795b9
JazminLuque <luque6170@gmail.com>
2023-09-12 18:17:15 -0300

    .

--- src/universidadGrupo57/UniversidadGrupo57.java
+++ src/universidadGrupo57/UniversidadGrupo57.java
@@ -5,14 +5,18 @@
  */
 package universidadGrupo57;
 
+import AccesoADatos.AlumnoData;
 import AccesoADatos.Conexion;
+import Entidades.Alumno;
 import java.sql.Connection;
 
 public class UniversidadGrupo57 {
 
     public static void main(String[] args) {
-      Connection con = Conexion.getConexion();
-    
+      //Connection con = Conexion.getConexion();
+    Alumno a= new Alumno(45866683, "Luque", "Jazmin",(2004-07-07), true);
+    AlumnoData data= new AlumnoData();
+    data.guardarAlumno(a);
     }
     
 }
