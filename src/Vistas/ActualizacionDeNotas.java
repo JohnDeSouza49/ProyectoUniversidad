0b68f0d62f9d5abd0c1ab7d7f574692c78a85711
JohnDeSouza49 <141689720+JohnDeSouza49@users.noreply.github.com>
2023-09-18 14:45:08 -0300

    hhh

--- src/Vistas/ActualizacionDeNotas.java
+++ src/Vistas/ActualizacionDeNotas.java
@@ -50,13 +50,13 @@ public class ActualizacionDeNotas extends javax.swing.JInternalFrame {
 
         jTNotaMateria.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Codigo", "Nombre", "Nota"
             }
         ));
         jScrollPane1.setViewportView(jTNotaMateria);
