a6c377cb0eb920b91752df5cc269bced08cd480c
MarcelaCerrudo <140634523+MarcelaCerrudo@users.noreply.github.com>
2023-09-19 12:41:52 -0300

    obtenerInscripciones

--- src/AccesoADatos/InscripcionData.java
+++ src/AccesoADatos/InscripcionData.java
@@ -19,24 +19,41 @@ public class InscripcionData {
         con = Conexion.getConexion();
     }
 
-    public void guardarInscripcion(int idAlumno, int idMateria)  throws SQLException {
+    public void guardarInscripcion(int idAlumno, int idMateria) throws SQLException {
         String sql = "INSERT INTO Inscripcion (idAlumno, idMateria) VALUES (?, ?)";
-         try (PreparedStatement ps = con.prepareStatement(sql)) {
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, idAlumno);
             ps.setInt(2, idMateria);
             ps.executeUpdate();
         } catch (SQLException e) {
             //JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno" + e.getMessage());
-             JOptionPane.showMessageDialog(null, "Error al guardar la inscripción"+ e.getMessage());
+            JOptionPane.showMessageDialog(null, "Error al guardar la inscripción" + e.getMessage());
         }
-       
+
     }
 
     public List<Inscripcion> obtenerInscripciones() {
 
         List<Inscripcion> inscripciones = new ArrayList<>();
+        String sql = "SELECT * FROM Inscripcion";
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {
+                int idInscripto = rs.getInt("idI");
+                double nota = rs.getInt("nota");
+               String materia = rs.getString("materia");
+                
+     Inscripcion inscripcion = new Inscripcion(idInscripto, nota, "materia");
+                inscripciones.add(inscripcion);
+            }
+        } catch (SQLException e) {
+           JOptionPane.showMessageDialog(null, "Error al obtener la lista" + e.getMessage());
+        }
 
         return inscripciones;
+        
+       
     }
 
     public List<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
@@ -70,15 +87,15 @@ public class InscripcionData {
 
     public List<Alumno> obtenerAlumnosXMateria(int idMateria) {
         List<Alumno> materiasXAlumno = new ArrayList<>();
-      String sql="SELECT * from alumno inner join inscripcion on(inscripcion.id_alumno=alumno.id_alumno) where id_materia=?";
-      PreparedStatement ps;
-      Alumno alumno= null;
+        String sql = "SELECT * from alumno inner join inscripcion on(inscripcion.id_alumno=alumno.id_alumno) where id_materia=?";
+        PreparedStatement ps;
+        Alumno alumno = null;
         try {
-            ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, idMateria);
-            ResultSet rs= ps.executeQuery();
-            
-          while(rs.next()){
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
                 alumno = new Alumno();
                 alumno.setIdAlumno(rs.getInt("id_alumno"));
                 alumno.setDni(rs.getInt("dni"));
@@ -88,18 +105,15 @@ public class InscripcionData {
                 alumno.setEstado(true);
                 materiasXAlumno.add(alumno);
                 ps.close();
-          }
-                
+            }
 
-            
         } catch (SQLException ex) {
             Logger.getLogger(InscripcionData.class.getName()).log(Level.SEVERE, null, ex);
         }
-      
-      
-        
 
         return materiasXAlumno;
     }
 
 }
+
+
